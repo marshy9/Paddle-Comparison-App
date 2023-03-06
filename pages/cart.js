1fadb1f9a3cbec15fba7f292a7015cb18633af12
Marshall <marshallhageman@gmail.com>
2023-03-05 18:53:46 -0600

    Working website from tutorial

--- pages/cart.js
+++ pages/cart.js
@@ -50,17 +50,16 @@ function CartScreen() {
                 {cartItems.map((item) => (
                   <tr key={item.slug} className="border-b">
                     <td>
-                      <Link href={`/product/${item.slug}`}>
-                        <a className="flex items-center">
-                          <Image
-                            src={item.image}
-                            alt={item.name}
-                            width={50}
-                            height={50}
-                          ></Image>
-                          &nbsp;
-                          {item.name}
-                        </a>
+                      <Link href={`/product/${item.slug}`} className="flex items-center">
+
+                        <Image
+                          src={item.image}
+                          alt={item.name}
+                          width={50}
+                          height={50}
+                        ></Image>
+                        {item.name}
+
                       </Link>
                     </td>
                     <td className="p-5 text-right">
