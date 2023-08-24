b632b2f15ee0f70854cfe2664d2d331e98d28550
Marshall <marshallhageman@gmail.com>
2023-08-23 21:32:44 -0500

    Modify color scheme

--- components/TabsBar.js
+++ components/TabsBar.js
@@ -1,4 +1,3 @@
-'use client';
 import { useState, useEffect, useRef } from 'react';
 
 const TabsBar = ({ items }) => {
@@ -10,30 +9,31 @@ const TabsBar = ({ items }) => {
   }, []);
 
   return (
-    <div className="bg-sky-100 flex justify-center items-center py-12">
-      <div className="max-w-md flex flex-col gap-y-2 w-full">
-        <div className="bg-blue-400 p-1  rounded-xl flex justify-between items-center gap-x-2 font-bold text-white">
-          {items.map((item, index) => (
-            <button
-              ref={index === 0 ? firstBtnRef : null}
-              key={index}
-              onClick={() => setSelectedTab(index)}
-              className={`outline-none w-full p-2 hover:bg-blue-300 rounded-xl text-cneter focus:ring-2 focus:bg-white focus:text-blue-600 ${
-                selectedTab === index ? 'ring-2 bg-white text-blue-600' : ''
-              } `}
-            >
-              {item.title}
-            </button>
-          ))}
-        </div>
+    <div className="max-w-md flex flex-col gap-y-2 w-full">
+      <div className="bg-gray-800 p-1 rounded-xl flex justify-between items-center gap-x-2 font-bold text-white">
+        {items.map((item, index) => (
+          <button
+            ref={index === 0 ? firstBtnRef : null}
+            key={index}
+            onClick={() => setSelectedTab(index)}
+            className={`outline-none w-full p-2 hover:bg-blue-300 rounded-xl text-center focus:ring-2 focus:bg-white focus:text-blue-600 ${
+              selectedTab === index ? 'ring-2 bg-white text-blue-600' : ''
+            } `}
+          >
+            {item.title}
+          </button>
+        ))}
+      </div>
 
-        <div className="bg-white p-2 rounded-xl">
-          {items.map((item, index) => (
-            <div className={`${selectedTab === index ? '' : 'hidden'}`}>
-              {item.content}
-            </div>
-          ))}
-        </div>
+      <div className="bg-white p-2 rounded-xl">
+        {items.map((item, index) => (
+          <div
+            className={`${selectedTab === index ? '' : 'hidden'}`}
+            key={index}
+          >
+            {item.content}
+          </div>
+        ))}
       </div>
     </div>
   );
