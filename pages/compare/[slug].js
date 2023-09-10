79265328b4f7c740d3932cdc8f7dc48d93a7cfb7
Marshall <marshallhageman@gmail.com>
2023-09-09 22:55:08 -0500

    working radar chart

--- pages/compare/[slug].js
+++ pages/compare/[slug].js
@@ -11,80 +11,64 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { FreeMode, Pagination } from 'swiper/modules';
 import TabsBar from '../../components/TabsBar';
 import SwiperItem from '../../components/SwiperItem';
-import {
-  Radar,
-  RadarChart,
-  PolarGrid,
-  Legend,
-  PolarAngleAxis,
-  PolarRadiusAxis,
-} from 'recharts';
-
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
+// Import radar charts
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Radar } from 'react-chartjs-2';
 
-const data = [
-  { subject: 'Math', A: 120, B: 110, fullMark: 150 },
-  { subject: 'Chinese', A: 98, B: 130, fullMark: 150 },
-  { subject: 'English', A: 86, B: 130, fullMark: 150 },
-  { subject: 'Geography', A: 99, B: 100, fullMark: 150 },
-  { subject: 'Physics', A: 85, B: 90, fullMark: 150 },
-  { subject: 'History', A: 65, B: 85, fullMark: 150 },
-];
-
-const items = [
-  {
-    title: 'Radar Chart',
-    content: (
-      <RadarChart
-        cx={300}
-        cy={250}
-        outerRadius={150}
-        width={500}
-        height={500}
-        data={data}
-      >
-        <PolarGrid gridType="circle" />
-        <PolarAngleAxis dataKey="subject" />
-        <PolarRadiusAxis angle={30} domain={[0, 150]} />
-        <Radar
-          name="Mike"
-          dataKey="A"
-          stroke="#8884d8"
-          fill="#8884d8"
-          fillOpacity={0.6}
-        />
-        <Radar
-          name="Lily"
-          dataKey="B"
-          stroke="#82ca9d"
-          fill="#82ca9d"
-          fillOpacity={0.6}
-        />
-        <Legend />
-      </RadarChart>
-    ),
-  },
-  {
-    title: 'Tab 2',
-    content: (
-      <div className="border-2 border-blue-400 rounded-lg p-4">
-        <h1 className="text-3xl text-blue-600">Title Test 2</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
-          aperiam asperiores dolo iti harum! Totam, mollitia quos voluptatem
-          deleniti provident obcaecati rerum.
-        </p>
-      </div>
-    ),
-  },
-];
+ChartJS.register(
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend
+);
 
 export default function PaddleScreen({ paddle }) {
   const { state, dispatch } = useContext(Store);
   const cartPaddles = state.cart.cartItems.slice(); // get full cart array
+
+  const data = {
+    labels: ['Price', 'RPM', 'Swing Weight'],
+
+    datasets: cartPaddles.map((paddle) => ({
+      label: paddle.name,
+      data: [paddle.price, paddle.rpm, paddle.swingWeight],
+      backgroundColor: 'rgba(255, 99, 132, 0.2)', // color
+    })),
+  };
+
+  const items = [
+    {
+      title: 'Radar Chart',
+      content: <Radar data={data} />,
+    },
+    {
+      title: 'Tab 2',
+      content: (
+        <div className="border-2 border-blue-400 rounded-lg p-4">
+          <h1 className="text-3xl text-blue-600">Title Test 2</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+            aperiam asperiores dolo iti harum! Totam, mollitia quos voluptatem
+            deleniti provident obcaecati rerum.
+          </p>
+        </div>
+      ),
+    },
+  ];
 
   // Add fixed width and height classes
   const boxStyles = 'w-64 h-64 ';
